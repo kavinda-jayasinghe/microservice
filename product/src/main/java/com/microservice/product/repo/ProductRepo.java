3be02fdfbf8226bcc32b63903d7205a05cbe8313
kavindasj-at-832266882009 <kavindasj@cf.lk>
2024-07-19 16:08:29 +0530

    first commit

---
+++ product/src/main/java/com/microservice/product/repo/ProductRepo.java
@@ -0,0 +1,13 @@
+package com.microservice.product.repo;
+
+
+import com.microservice.product.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Integer> {
+    @Query(value = "SELECT * FROM product WHERE id = ?1", nativeQuery = true)
+    Product getProductById(Integer productId);
+}
